2c40d7b121138ae365bb3b0bb108d1a89b352246
David Neto <dneto@google.com>
2017-04-28 09:34:05 -0400

    Extract names of entry points

--- examples/descriptors/entry_point_info.cpp
+++ examples/descriptors/entry_point_info.cpp
@@ -14,7 +14,53 @@
 
 #include "entry_point_info.h"
 
+#include <cassert>
+#include <utility>
+#include <vector>
+
+#include "spirv/1.1/spirv.h"
 #include "spirv-tools/libspirv.h"
+
+using spirv_example::EntryPointInfo;
+
+namespace {
+
+// Returns a string for the literal string logical operand starting at the
+// given word.  assumes it has a terminating null as required.
+std::string StringFromWords(const uint32_t* words) {
+  return reinterpret_cast<const char*>(words);
+}
+
+// holds accumulated information about all entry points in the module.
+class Collector {
+ public:
+  explicit Collector(std::vector<EntryPointInfo>* entry_points)
+      : entry_points_(entry_points) {
+    entry_points_->clear();
+  }
+
+  spv_result_t HandleInstruction(const spv_parsed_instruction_t& inst) {
+    if (inst.opcode == SpvOpEntryPoint) {
+      entry_points_->push_back(EntryPointInfo{StringFromWords(inst.words + 3)});
+    }
+    return SPV_SUCCESS;
+  }
+
+ private:
+  // The accumulated entry point information.
+  std::vector<EntryPointInfo>* entry_points_;
+};
+
+// Binary parser handle-instruction callback.
+// Captures necessary information from an instruction, assuming they are
+// seen in order.
+spv_result_t HandleInstruction(
+    void* user_data, const spv_parsed_instruction_t* parsed_instruction) {
+  assert(user_data);
+  auto collector = static_cast<Collector*>(user_data);
+  return collector->HandleInstruction(*parsed_instruction);
+}
+}
 
 namespace spirv_example {
 
@@ -23,10 +69,9 @@ spv_result_t GetEntryPointInfo(const spv_const_context context,
                                std::vector<EntryPointInfo>* entry_points,
                                spv_diagnostic* diagnostic) {
   if (!entry_points) return SPV_ERROR_INVALID_POINTER;
-  entry_points.clear();
-  auto status = spvBinaryParse(context, nullptr, words, num_words, nullptr,
-                               nullptr, diagnostic);
-
+  Collector collector(entry_points);
+  auto status = spvBinaryParse(context, &collector, words, num_words, nullptr,
+                               HandleInstruction, diagnostic);
   return status;
 }
 
