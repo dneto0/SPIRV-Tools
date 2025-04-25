c4bc3f11708480e1da177aaa90fd63e8e6c836d9
dneto <dneto@google.com>
2025-04-25 18:33:42 -0400

    Checkpoint: add a comment

--- source/table2.cpp
+++ source/table2.cpp
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Compressed grammar tables.
+
 #include "source/table2.h"
 
 #include <algorithm>
